fd8c4e3be134aff3e8b9f4cc684e242e5dabb5d3
yess-wee <yashvichauhan83@gmail.com>
2024-03-02 22:30:09 +0530

    created file share module in which i've made AES and DES functionality to make permission while sharing and recieving pdf.... Completed sharing part...

---
+++ app/src/main/java/com/example/fileshare/adapter/ShowAllReceivedFiles.java
@@ -0,0 +1,72 @@
+package com.example.fileshare.adapter;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fileshare.R;
+import com.example.fileshare.model.FileShared;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowAllReceivedFiles extends AppCompatActivity {
+
+    RecyclerView rv_fileShared;
+    String username, id, email, image, sender;
+
+    List<FileShared> fileSharedList = new ArrayList<>();
+    ShowFileReceivedAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all_received_files);
+
+        rv_fileShared = findViewById(R.id.rv_fileShared);
+        rv_fileShared.setHasFixedSize(true);
+        rv_fileShared.setLayoutManager(new LinearLayoutManager(ShowAllReceivedFiles.this));
+
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+
+        getFileShared();
+
+    }
+
+    private void getFileShared() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser.getUid() != null) {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child(UserRegister.FILE_RECEIVED).child(username);
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    fileSharedList.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        FileShared model = dataSnapshot.getValue(FileShared.class);
+                        fileSharedList.add(model);
+                    }
+                    adapter = new ShowFileReceivedAdapter(ShowAllReceivedFiles.this, fileSharedList);
+                    rv_fileShared.setAdapter(adapter);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+    }
+
+}
